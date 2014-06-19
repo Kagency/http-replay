010f2eac551de7be8393b1eea57d9b0e40ecf51a
Kore Nordmann <mail@kore-nordmann.de>
2014-06-19 15:00:29 +0200

    Implemented JSON filter

---
+++ src/Kagency/HttpReplay/ResponseFilter/JsonFilter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Kagency\HttpReplay\ResponseFilter;
+
+use Kagency\HttpReplay\ResponseFilter;
+use Kagency\HttpReplay\SimplifiedResponse;
+
+/**
+ * Class: JsonFilter
+ *
+ * @IDEA Make it possible to filter deep properties with something like
+ * JSON-path.
+ *
+ * @version $Revision$
+ */
+class JsonFilter extends ResponseFilter
+{
+    /**
+     * Json property blacklist
+     *
+     * @var string[]
+     */
+    private $blacklist = array();
+
+    /**
+     * __construct
+     *
+     * @param string[] $blacklist
+     * @return void
+     */
+    public function __construct(array $blacklist = array())
+    {
+        $this->blacklist = $blacklist;
+    }
+
+    /**
+     * Filter response
+     *
+     * @param SimplifiedResponse $response
+     * @return void
+     */
+    public function filterResponse(SimplifiedResponse $response)
+    {
+        if (!is_array($response->content)) {
+            throw new \UnexpectedValueException("Expecting a parsed JSON response.");
+        }
+
+        $response->content = array_diff_key(
+            $response->content,
+            array_flip($this->blacklist)
+        );
+    }
+}
---
+++ tests/Kagency/HttpReplay/ResponseFilter/JsonFilterTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Kagency\HttpReplay\ResponseFilter;
+
+use Kagency\HttpReplay\SimplifiedResponse;
+
+class JsonFilterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFilterProperty()
+    {
+        $filter = new JsonFilter(array('bar'));
+
+        $response = new SimplifiedResponse(
+            '/',
+            200,
+            array(),
+            array(
+                'foo' => 'bar',
+                'bar' => 'blubb',
+            )
+        );
+        $filter->filterResponse($response);
+
+        $this->assertSame(
+            array(
+                'foo' => 'bar',
+            ),
+            $response->content
+        );
+    }
+
+    public function testFilterInexistantProperty()
+    {
+        $filter = new JsonFilter(array('inexistant'));
+
+        $response = new SimplifiedResponse(
+            '/',
+            200,
+            array(),
+            array(
+                'bar' => 'blubb',
+            )
+        );
+        $filter->filterResponse($response);
+
+        $this->assertSame(
+            array(
+                'bar' => 'blubb',
+            ),
+            $response->content
+        );
+    }
+
+    /**
+     * @expectedException UnexpectedValueException
+     */
+    public function testFilterInvalidContent()
+    {
+        $filter = new JsonFilter(array('Foo'));
+
+        $response = new SimplifiedResponse(
+            '/',
+            200,
+            array(),
+            null
+        );
+        $filter->filterResponse($response);
+    }
+}
